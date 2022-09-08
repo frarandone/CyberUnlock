1ed74cb9d090244f903a72e9d69bbf17f4d02df0
frarandone <frarandone@gmail.com>
2022-09-08 14:55:09 +0200

    Adapted for single teams

--- LineEdit.gd
+++ LineEdit.gd
@@ -3,7 +3,7 @@ extends LineEdit
 var code
 var prefix
 var msg
-var prefixes = ['tec', 'sys', 'cyb', 'hac', 'ufo', 'squ', 'non', 'cob', 'boo', 'wor', 'cod']
+#var prefixes = ['tec', 'sys', 'cyb', 'hac', 'ufo', 'squ', 'non', 'cob', 'boo', 'wor', 'cod']
 
 func _ready():
 	connect("focus_entered", self, "js_text_entry")
@@ -19,45 +19,24 @@ func _on_LineEdit_text_entered():
 	code = text
 	code = code.replace(' ', '')
 	code = code.to_lower()
-	prefix = code.substr(0,3)
-	msg = code.substr(3,-1)
-	check_message(prefix, msg)
+	#prefix = code.substr(0,3)
+	#msg = code.substr(3,-1)
+	check_message(code)
 	
 	
-func check_message(prefix, msg):
-	if prefixes.has(prefix) and msg=='3784':
+func check_message(msg):
+	if msg=='3784':
 		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel8021").set_visible(true)
-	elif prefixes.has(prefix) and msg=='2002':
+	elif msg=='2002':
 		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel0473").set_visible(true)
-	elif prefixes.has(prefix) and msg=='off_on_on_off_off':
+	elif msg=='off_on_on_off_off':
 		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4785").set_visible(true)
-	elif prefixes.has(prefix) and msg=='4058':
-		return_letter(prefix)
-	elif msg=='137':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Final").set_visible(true)
+	elif msg=='4058':
+		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060").set_visible(true)
 	else:
 		get_tree().get_root().get_node("/root/Node2D/Background/Panels/PanelError").set_visible(true)
 		
-func return_letter(prefix):
-	get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060").set_visible(true)
-	if prefix == 'tec':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/U").set_visible(true)
-	if prefix == 'sys':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/N").set_visible(true)
-	if prefix == 'cyb':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/O").set_visible(true)		
-	if prefix == 'hac' or prefix=='boo' or prefix=='wor':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/T").set_visible(true)		
-	if prefix == 'ufo':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/R").set_visible(true)		
-	if prefix == 'squ' or prefix=='cob' or prefix=='cod':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/E").set_visible(true)		
-	if prefix == 'non':
-		get_tree().get_root().get_node("/root/Node2D/Background/Panels/Panel4060/S").set_visible(true)		
 		
-
-
-
 func _on_TextureButton_pressed():
 	get_tree().get_root().get_node("/root/Node2D/Background/LineEdit")._on_LineEdit_text_entered()
 		
